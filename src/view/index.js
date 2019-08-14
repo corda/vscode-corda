a811dd486bc08482c6db343f620aacd08eaa979f
FreyaRhianna <freyarsh@gmail.com>
2019-08-14 17:17:57 +0100

    Updated views for splash view

--- src/view/index.js
+++ src/view/index.js
@@ -4,8 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import VaultView from './VaultView';
+import SplashPage from './SplashPage';
 
-ReactDOM.render(<VaultView />, document.getElementById('root'));
+ReactDOM.render(<SplashPage />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
