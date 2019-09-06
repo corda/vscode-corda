8ae293d8b7f24201f578ded4ca1a74b68be71f7d
FreyaRhianna <freyarsh@gmail.com>
2019-09-06 08:59:44 +0100

    Fixed miscoloured icon on vault display

--- src/view/component-view/VaultTransactionDisplay.js
+++ src/view/component-view/VaultTransactionDisplay.js
@@ -108,6 +108,9 @@ export default class VaultTransactionDisplay extends React.Component {
             classes: {
                 paper: "selection-box-dropdown"
             }
+        },
+        classes : {
+          icon: "selection-box-icon"
         }
       };
         this.sortTable(this.state.transactionMap, this.state.asc);
