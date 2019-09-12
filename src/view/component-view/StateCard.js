68491f3477d3ad9a27abb1d225d1a030236a9b6a
FreyaRhianna <freyarsh@gmail.com>
2019-09-12 11:46:30 +0100

    added select boxes for stateref and linearids

--- src/view/component-view/StateCard.js
+++ src/view/component-view/StateCard.js
@@ -15,7 +15,11 @@ export default class StateCard extends React.Component {
             stateData : props.stateData
           
         }
+     
 
+        this.copyToClipboard = this.copyToClipboard.bind(this);
+        this.updateClipboard = this.updateClipboard.bind(this)
+        this.updateClipboard(props);
 
     }
 
@@ -26,28 +30,61 @@ export default class StateCard extends React.Component {
         }
     }
 
+    updateClipboard(props){
+        if(props.stateData.linearId){
+            this.state.linearID = Object.keys(props.stateData.linearId).map(function(key) {
+                return props.stateData.linearId[key];
+            })[0]
+        } 
+        this.state.stateRef = props.metaData.stateRef;
+    }
 
+    copyToClipboard(e, property){
+        var textArea = document.createElement("textarea");
+        textArea.value = this.state[property];
+        textArea.style.width = "0px"
+        textArea.style.height = "0px"
+        this.stateCard.appendChild(textArea);
+        textArea.select();
+
+        try {
+            var successful = document.execCommand('copy');
+            var msg = successful ? 'successful' : 'unsuccessful';
+            console.log('Copying text command was ' + msg);
+        } catch (err) {
+            console.log('Unable to copy');
+        }
+
+        this.stateCard.removeChild(textArea);
+        
+    }
 
    
 
     
     render() {
+        const copyButton = <span> {this.state.metaData.stateRef}<span onClick={(e) => this.copyToClipboard(e, "stateRef")} class="copy-button-container">< i class="fas fa-copy copy-button" ></i> </span></ span>
         
         return(
-           <Card className="state-card">
+           <Card className="state-card"  ref={(stateCard) => this.stateCard = stateCard}>
                <CardHeader
                 
                
                 title= {this.state.metaData.stateType}
-                subheader={this.state.metaData.stateRef}
+                subheader={copyButton } 
             />
-               <CardContent>
-                   {Object.keys(this.state.stateData).map((property,index) => (
-                       <div key={"stateProperty" + index}>
-                           <em> {property}</em> : {JSON.stringify(this.state.stateData[property])}
-                       </div>
 
-                   ))}
+               <CardContent>
+                   {Object.keys(this.state.stateData).map((property,index) => {
+                       var copyButton = null;
+                       if(property == "linearId"){
+                            copyButton = <span onClick={(e) => this.copyToClipboard(e, "linearID")} class="copy-button-container">< i class="fas fa-copy copy-button" ></i> </span>
+                       }
+                       return(<div key={"stateProperty" + index}>
+                           <em> {property}</em> : {JSON.stringify(this.state.stateData[property])} {copyButton}
+                       </div>);
+
+                   })}
                </CardContent>
            </Card>
         );
