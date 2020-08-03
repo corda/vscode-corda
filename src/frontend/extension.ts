2fae2767b59f3d7760dbf366ac922d8992ff62bd
nikolai <nikolaimerritt20@gmail.com>
2020-08-03 13:40:58 +0100

    test commit (yet again)

--- src/frontend/extension.ts
+++ src/frontend/extension.ts
@@ -35,7 +35,7 @@ const getStaticLogWebViewContent = (text: string) =>
 	`<!DOCTYPE html>
 	<html lang="en">
 	<head> 
-		<meta charset="UTF-8">
+		<meta charset="UTF-8"> 
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>A Static Webview</title>
 	</head>
