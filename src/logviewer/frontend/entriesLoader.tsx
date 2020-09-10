cc50e262d03cae8d6e98b4ffc518de5b06258cb6
Nikolai Merritt <nikolaimerritt20junk@gmail.com>
2020-09-10 11:04:20 +0100

    Set entry.body.object to be object instead of JSON string
    Accepted changes

--- src/logviewer/frontend/entriesLoader.tsx
+++ src/logviewer/frontend/entriesLoader.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
-import InfiniteLoader from "react-window-infinite-loader";
-import { VariableSizeList as List } from "react-window";
-import { LogEntry, sampleLogEntry } from "../backend/types";
+import { InfiniteLoader, List, IndexRange, ListRowProps, AutoSizer } from "react-virtualized";
+import { LogEntry } from "../backend/types";
 import * as util from "../backend/util";
 import axios from "axios";
 import { EntryButton, LoadingEntry } from "./entry";
@@ -12,10 +10,19 @@ type filterType = (entry: LogEntry) => boolean;
 export const EntriesLoader = (props: {filepath: string, amountOfEntries: number, filterBy: filterType}) => {
     let entries = new Array<LogEntry>();
 
-    const isItemLoaded = (index: number) => entries[index] !== undefined;
+    const isRowLoaded = (index: number) => entries[index] !== undefined;
+
+    const tidyEntry = (entry: any) => ({
+        ...entry, 
+        date: new Date(util.before(entry.date, ",")),
+        body: {
+            ...entry.body,
+            object: JSON.parse(entry.body.object)
+        }
+    })
 
     // [startIndex, stopIndex)
-    const loadMoreItems = async (startIndex: number, stopIndex: number) => {
+    const loadMoreRows = async (startIndex: number, stopIndex: number) => {
         const newEntries = (await axios.post("http://localhost:8580/logReader/read", {
             startIndex,
             stopIndex,
@@ -27,29 +34,39 @@ export const EntriesLoader = (props: {filepath: string, amountOfEntries: number,
         entries = util.placeAt(entries, newEntries, startIndex);
     }
 
-    const Row = ({index, style}) =>
-        isItemLoaded(index) ? 
-            <EntryButton entry={entries[index]} key={index}/> :
-            <LoadingEntry key={index}/>
+    const rowRenderer = ({key, index, style}: ListRowProps) => {
+        if (isRowLoaded(index)) {
+            if (props.filterBy(entries[index])) {
+                return <EntryButton entry={entries[index]} key={key} />
+            }
+            return <div style={{display: "none"}}> I'm invisible! </div>;
+        }
+        return <LoadingEntry key={key}/>
+    }
+        
 
     return (
         <InfiniteLoader
-            isItemLoaded = {isItemLoaded}
-            loadMoreItems = {loadMoreItems}
-            itemCount = {props.amountOfEntries}
+            isRowLoaded = {({index}) => isRowLoaded(index)}
+            loadMoreRows = {loadMoreRows}
+            rowCount = {props.amountOfEntries}
         >
-            {({ onItemsRendered, ref }) => (
-                <List
-                    onItemsRendered={onItemsRendered}
-                    ref={ref}
-                    height={600}
-                    itemCount={props.amountOfEntries}
-                    itemSize={(index: number) => index % 2 === 0 ? 50 : 100}
-                    width={300}
-                >
-                    {Row}
-                </List>
-            )}
+            {({ onRowsRendered, registerChild }) =>
+                <AutoSizer>
+                    {({width, height}) => 
+                        <List
+                            style={{background: "blue"}}
+                            height={height}
+                            width={width}
+                            onRowsRendered={onRowsRendered}
+                            ref={registerChild}
+                            rowCount={props.amountOfEntries}
+                            rowRenderer={rowRenderer}
+                            rowHeight={1 + height / props.amountOfEntries}
+                        />
+                    }
+                </AutoSizer>
+            }
         </InfiniteLoader>
     )
 }
