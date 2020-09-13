7a0ec5bcabef0d47b129e5d771a2524d68ffc0df
Anthony Nixon <anixon604@gmail.com>
2020-09-13 20:01:38 +0100

    BaseContractState template added to ./resources

---
+++ resources/BaseContractState.java
@@ -0,0 +1,17 @@
+import net.corda.core.contracts.ContractState;
+import net.corda.core.identity.AbstractParty;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class BaseContractState implements ContractState {
+
+    public BaseContractState() {
+    }
+
+    @NotNull
+    @Override
+    public List<AbstractParty> getParticipants() {
+        return null;
+    }
+}
