683a1897cee3032ac4572cb6bd520205d6b576c1
Anthony Nixon <anixon604@gmail.com>
2020-09-27 22:33:27 +0100

    bug in watcher argument corrected

--- src/watchers.ts
+++ src/watchers.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 
 // Watcher for gradle.build refresh
 export const getBuildGradleFSWatcher = () => {
-    let pattern = new vscode.RelativePattern(vscode.workspace.getWorkspaceFolder[0], '**/*.gradle');
+    let pattern = new vscode.RelativePattern(vscode.workspace.workspaceFolders![0].uri.toString(), '**/*.gradle');
     let watcher = vscode.workspace.createFileSystemWatcher(pattern);
     watcher.onDidChange((event) => { 
         watcher.dispose();
