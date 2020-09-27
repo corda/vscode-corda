51b1b8fd32c33415e8273386db32e355f731a6c5
Anthony Nixon <anixon604@gmail.com>
2020-09-27 18:56:14 +0100

    types added for runningNodes

--- src/types.ts
+++ src/types.ts
@@ -7,32 +7,37 @@ export interface CordaNode {
     p2pPort: string,
     rpcSettings: any,
     rpcUsers: any,
-    cordappDir: string;
+    jarDir: string;
 }
 
+// PARSING
 // tslint:disable-next-line: class-name
 export interface CordaNodeConfig {
 	[index: number]: CordaNode
 }
 
+// PARSING
 // tslint:disable-next-line: class-name
 export interface CordaNodesDefaultConfig{
 	rpcUsers: any;
 }
 
+// PARSING
 // tslint:disable-next-line: class-name
 export interface CordaNodesConfig {
 	node: CordaNodeConfig;
 	nodeDefaults: CordaNodesDefaultConfig;
 }
 
-
+// PARSING
 // tslint:disable-next-line: class-name
 export interface CordaTaskConfig {
     file: string,
 	task: CordaNodesConfig;
 }
 
+// Login Request to connect via RPC
+// TODO: add CorDapp Dir to LoginRequest
 export interface LoginRequest {
     hostName: string,
     port: string,
@@ -40,19 +45,30 @@ export interface LoginRequest {
     password: string,
 }
 
+// Instance of a node that is currently running its Corda.jar
+// return UUID RPCClientId from login result
 export interface RunningNode {
     id: string,
-    deployedNode: DeployedNode,
+    rpcClientId: string | undefined, // unique ID For routing to proper rpc-connection
+    deployedNode: DefinedNode,
     terminal: vscode.Terminal
 }
 
- export interface DeployedNode {
-     loginRequest: LoginRequest,
-     id: string,
-     x500: {
-         name: string,
-         city: string,
-         country: string
-     },
-     nodeConf: CordaNode
- }
+// Dictionary of running nodes key'd by WORKSPACE
+export interface RunningNodesList {
+    //associated workspace/project
+    [workspaceName: string]: {runningNodes: RunningNode[]}
+}
+
+// An instance of a valid NODE in this project
+export interface DefinedNode {
+    loginRequest: LoginRequest,
+    id: string,
+    rpcPort: string,
+    x500: {
+        name: string,
+        city: string,
+        country: string
+    },
+    nodeConf: CordaNode
+}
