5d00c6b429f46e5ed403649260a216c1675046de
Anthony Nixon <anixon604@gmail.com>
2020-09-27 18:55:02 +0100

    added constancts for workspaceState keys

--- src/CONSTANTS.ts
+++ src/CONSTANTS.ts
@@ -2,10 +2,10 @@
  * Constants for Inherited Corda Core Types
  */
 export abstract class Constants {
-    static readonly contractStateBaseInterfaces = ['ContractState', 'FungibleState', 'LinearState', 'OwnableState', 'QueryableState', 'SchedulableState'];
-    static readonly contractBaseInterface = ['Contract'];
-	static readonly flowBaseClass = ['FlowLogic'];
-	static readonly gitHubApi = {
+    static readonly CONTRACTSTATE_BASE_INTERFACES = ['ContractState', 'FungibleState', 'LinearState', 'OwnableState', 'QueryableState', 'SchedulableState'];
+    static readonly CONTRACT_BASE_INTERFACE = ['Contract'];
+	static readonly FLOW_BASE_CLASS = ['FlowLogic'];
+	static readonly GITHUB_API = {
         'cordapp-template-java':'https://api.github.com/repos/corda/cordapp-template-java/zipball',
         'bootcamp-cordapp':'https://api.github.com/repos/corda/bootcamp-cordapp/zipball'
     };
@@ -13,7 +13,8 @@ export abstract class Constants {
 
 export const GRADLE_TASKS_EXTENSION_ID = 'richardwillis.vscode-gradle';
 export const SERVER_BASE_URL = 'http://localhost:8580';
-export const SERVER_JAR = "explorer-server-0.1.0.jar";
+export const SERVER_JAR = 'explorer-server-0.1.0.jar';
+export const RUN_CORDA_CMD = 'java -jar corda.jar';
 
 export abstract class WorkStateKeys {
     static readonly PROJECT_IS_CORDA = 'projectIsCorda';
@@ -21,6 +22,14 @@ export abstract class WorkStateKeys {
     static readonly DEPLOY_NODES_BUILD_GRADLE = 'deployNodesBuildGradle';
     static readonly ARE_NODES_DEPLOYED = 'areNodesDeployed';
     static readonly IS_NETWORK_RUNNING = 'isNetworkRunning';
+
+    static readonly ALL_KEYS = [
+        WorkStateKeys.PROJECT_IS_CORDA,
+        WorkStateKeys.DEPLOY_NODES_LIST, 
+        WorkStateKeys.DEPLOY_NODES_BUILD_GRADLE,
+        WorkStateKeys.ARE_NODES_DEPLOYED,
+        WorkStateKeys.IS_NETWORK_RUNNING
+    ]
 }
 
 export abstract class GlobalStateKeys {
